d869c6b0d27fe512ef79204dec8ed9a6ab588e03
ggodreau <godreau@gmail.com>
2019-01-24 12:03:08 -0600

    init
      - vanilla implementation of the gatsby-starter template
      - hard coded height for page
      - black title bar in meta tag within index.js jsx

---
+++ README.md
@@ -0,0 +1,7 @@
+<!-- AUTO-GENERATED-CONTENT:START (STARTER) -->
+<h1 align="center">
+  Metro App
+</h1>
+
+Mockup with react
+
---
+++ src/components/layout.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Header from './header'
+import './layout.css'
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div>new div</div>
+        {/*This is the main body*/}
+        {/*padding is above left below*/}
+        <div
+          style={{
+            margin: `0 auto`,
+            color: `coral`,
+            maxWidth: 960,
+            minHeight: 600,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          {children}
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
