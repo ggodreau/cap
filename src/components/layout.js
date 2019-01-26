a72be9decdce973e984274ef2b9cabddfd8a0a22
ggodreau <godreau@gmail.com>
2019-01-26 07:53:54 -0600

    working great with trollfacelol img

--- src/components/layout.js
+++ src/components/layout.js
@@ -32,9 +32,12 @@ const Layout = ({ children }) => (
         <div id="middle">
           <div className="bstripe-top" />
           <div className="cap-logo">
-            <svg height="210" width="400">
-              <path d="M150 0 L75 200 L225 200 Z" />
-            </svg>
+            <img
+              src="http://www.stickpng.com/assets/images/580b585b2edbce24c47b2a2f.png"
+              height="75px"
+              width="75px"
+              alt="lolol"
+            />
           </div>
           <div className="bstripe-bot" />
           <div className="mid-text">1 LOCAL</div>
