c09d93e6aaf92454558cfb3569b12397fdce7813
ggodreau <godreau@gmail.com>
2019-01-26 07:21:36 -0600

    draft of svg working wiht layers correct, window scroll is still a prob

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,33 +18,33 @@ const Layout = ({ children }) => (
       <div id="parent">
         <div id="header" />
         <div id="upper">
-          <div class="ustripe-top-bg" />
-          <div class="ustripe-top" />
-          <div class="ustripe-bot-bg" />
-          <div class="ustripe-bot" />
-          <div class="tick-top" />
-          <div class="tick-bot" />
-          <div class="notch-top" />
-          <div class="notch-bot" />
-          <div class="cap-text">CAPITAL METRO</div>
+          <div className="ustripe-top-bg" />
+          <div className="ustripe-top" />
+          <div className="ustripe-bot-bg" />
+          <div className="ustripe-bot" />
+          <div className="tick-top" />
+          <div className="tick-bot" />
+          <div className="notch-top" />
+          <div className="notch-bot" />
+          <div className="cap-text">CAPITAL METRO</div>
         </div>
         <div id="break" />
         <div id="middle">
-          <div class="bstripe-top" />
-          <div class="cap-logo">
+          <div className="bstripe-top" />
+          <div className="cap-logo">
             <svg height="210" width="400">
               <path d="M150 0 L75 200 L225 200 Z" />
             </svg>
           </div>
-          <div class="bstripe-bot" />
-          <div class="mid-text">1 LOCAL</div>
+          <div className="bstripe-bot" />
+          <div className="mid-text">1 LOCAL</div>
         </div>
         <div id="break" />
         <div id="bottom">
-          <div class="fstripe-top" />
-          <div class="fstripe-bot" />
-          <div class="bot-text shim1">01/23/2019 at 7:57AM</div>
-          <div class="bot-text shim2">Ticket Expiration Date</div>
+          <div className="fstripe-top" />
+          <div className="fstripe-bot" />
+          <div className="bot-text shim1">01/23/2019 at 7:57AM</div>
+          <div className="bot-text shim2">Ticket Expiration Date</div>
         </div>
       </div>
     )}
